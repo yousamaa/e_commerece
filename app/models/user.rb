45f5c455ab6b82a8a581449fdb987e8e9c6cbc33
Muhammad Usama Yousaf <109678350+yousamaaa@users.noreply.github.com>
2022-08-26 22:06:19 +0500

    MODEL ADDED::PRODUCT (#8)
    * Generated product model
    * Added has_many :products association to user model
    * Added belongs_to :user association to product model
    * Added has_many_attached association of active storage in product model
    * Added user_id in products table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+  has_many :products
 
   after_commit :add_default_avatar, on: %i[create update]
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_26_144558) do
+ActiveRecord::Schema.define(version: 2022_08_26_164558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2022_08_26_144558) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -50,4 +59,5 @@ ActiveRecord::Schema.define(version: 2022_08_26_144558) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "products", "users"
 end
