200438a5faf1b4b0984bb3ddf433d89bc92b4abf
yousamaaa <usama.yousaf@devsinc.com>
2022-09-05 12:10:14 +0500

    RUBOCOP CORRCTIONS
    * Guard clauses corrections
    * dependant options corrections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,9 +19,9 @@ class ApplicationController < ActionController::Base
 
   def initialize_cart
     @cart ||= Cart.find_by(id: session[:cart_id])
-    if @cart.nil?
-      @cart = Cart.create
-      session[:cart_id] = @cart.id
-    end
+    return @cart unless @cart.nil?
+
+    @cart = Cart.create
+    session[:cart_id] = @cart.id
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
-  has_many :products
-  has_many :comments
-  has_one :cart
+  has_many :products, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_one :cart, dependent: :destroy
   validates :name, :email, presence: true
 
   after_commit :add_default_avatar, on: %i[create update]
@@ -24,14 +24,14 @@ class User < ApplicationRecord
   private
 
   def add_default_avatar
-    unless avatar.attached?
-      avatar.attach(
-        io: File.open(
-          Rails.root.join(
-            'app', 'assets', 'images', 'default_profile.jpg'
-          )
-        ), filename: 'default_profile.jpg', content_type: 'image/jpg'
-      )
-    end
+    return if avatar.attached?
+
+    avatar.attach(
+      io: File.open(
+        Rails.root.join(
+          'app', 'assets', 'images', 'default_profile.jpg'
+        )
+      ), filename: 'default_profile.jpg', content_type: 'image/jpg'
+    )
   end
 end
