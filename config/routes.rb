43a88b2f3f0e5487bbdd59f9294f9cec7136c414
yousamaaa <usama.yousaf@devsinc.com>
2022-10-13 21:19:16 +0500

    ADDED SERIALIZERS FOR PRODUCT INDEX AND SHOW

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,11 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  namespace :api do
+    namespace :v1 do
+      resources :products, only: [:index, :show]
+    end
+  end
+
   get '*path' => redirect('/')
 end
